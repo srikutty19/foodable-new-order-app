635a79f640d9e8fbb08562a2414e2833ffaa512d
srikutty19 <sri9132@gmail.com>
2019-03-21 16:18:08 +0530

    first commit

---
+++ www/js/config.js
@@ -0,0 +1,7 @@
+
+var krms_config ={			
+	'ApiUrl' : "http://rapidtest.ga/restomulti/merchantapp/api",	
+	'DialogDefaultTitle' : " Foodable MERCHANT",
+	'pushNotificationSenderid' : "694762840152",
+	'APIHasKey' : "e7fd6af77a1d45f791880ddebaee29f4"
+};
