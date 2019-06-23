483832b51c2d65ba186ff3aa9a75f1c361f5f9ee
srikutty19 <sri9132@gmail.com>
2019-06-23 09:04:40 +0530

    Update config.js

--- www/js/config.js
+++ www/js/config.js
@@ -1,7 +1,7 @@
 
 var krms_config ={			
-	'ApiUrl' : "http://athavtech.com/AppProjects/foodable/merchantapp/api",	
+	'ApiUrl' : "http://foodable.athavtech.com/merchantapp/api",	
 	'DialogDefaultTitle' : " Foodable MERCHANT",
 	'pushNotificationSenderid' : "694762840152",
-	'APIHasKey' : "53aaa994fe6a6ff0f6f141e9e6468b67"
+	'APIHasKey' : "bd35f2ea56fd1785a9756a350bac7f44"
 };
